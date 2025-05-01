08088ae1d61cc0b3238a56bf620b2d35f50a96f3
linda-aleman <alemanlinda2007@gmail.com>
2025-05-01 15:07:09 -0600

    cambio de mensaje(saludo) principal de la pagina de la calculadora

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from operacion import separar, numeros, operar
 
 app = Flask(__name__)
 
-saludo = "Bienvenido esto es una calculadora realizada con Flask  en python"
+saludo = "Bienvenido a la calculadora realizada con Flask  en python"
 @app.route('/')
 def index():
     return render_template('index.html', saludo=saludo)
@@ -11,10 +11,10 @@ def index():
 @app.route('/calculadora' , methods=['GET', 'POST'])
 def calculadora():
     operacion = request.form.get('pantalla')
-    bloques = separar(operacion)
-    transformar = numeros(bloques)
-    resultado = operar(transformar)
-    print(resultado)
+    bloques = separar (operacion)
+    transformar = numeros (bloques)
+    resultado = operar (transformar)
+    print (resultado)
     return render_template(
         'calculadora.html',
         bloques=bloques,  
@@ -25,5 +25,5 @@ def calculadora():
 
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(debug=True)
